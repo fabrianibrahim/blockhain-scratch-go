2372145ceef6594c0f1aa52c90494134add45fbf
Fabrian Ibrahim <fabrian.ibrahim@gmail.com>
2024-05-15 22:13:18 +1000

    add wallet

--- main.go
+++ main.go
@@ -6,28 +6,62 @@ import (
 	"blockChain/blockchain"
 )
 
+
 func main() {
-	fmt.Println("Blockchain from Scratch")
-	chain := blockchain.InitBlockChain()
-	chain.AddBlock("Block 1", "Alice", []*blockchain.Transaction{
-        {Sender: "Alice", Receiver: "Bob", Amount: 1.5},
-        {Sender: "Alice", Receiver: "Charlie", Amount: 19.5},
-    })
+    chain := blockchain.InitBlockChain()
+    aliceWallet, err := blockchain.NewWallet()
+    
+    if err != nil {
+        fmt.Println("Error creating Alice's wallet:", err)
+        return
+    }
+    fmt.Println("Alice's wallet created successfully")
 
-    chain.AddBlock("Block 2", "Bob", []*blockchain.Transaction{
-        {Sender: "Bob", Receiver: "Charlie", Amount: 2.3},
-    })
+    bobWallet, err := blockchain.NewWallet()    
+    if err != nil {
+        fmt.Println("Error creating Bob's wallet:", err)
+        return
+    }
+    fmt.Println("Bob's wallet created successfully")
 
-	for _, block := range chain.Blocks {
-		fmt.Printf("Previous hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash of block: %x\n", block.Hash)
+    tx := &blockchain.Transaction{
+        Sender:   aliceWallet.PublicKey.N.String(),
+        Receiver: bobWallet.PublicKey.N.String(),
+        Amount:   5.0,
+    }
+    fmt.Println("Alice to Bob Transaction created successfully")
 
-		pow := blockchain.NewProofOfWork(block)
-		fmt.Printf("IsValidPow: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+    signature, err := aliceWallet.SignTransaction(tx)
+    if err != nil {
+        fmt.Println("Error signing the transaction:", err)
+        return
+    }
 
-		fmt.Println("Transactions:")
+    // Verify the transaction using Alice’s wallet, public key, and the signature.
+    err = blockchain.VerifyTransaction(tx, aliceWallet.PublicKey, signature)
+    
+    if err != nil {
+        fmt.Println("Transaction verification failed:", err)
+        return
+    }
+
+    fmt.Println("Transaction Verified Successfully")
+
+    // Add the verified transaction to the blockchain.
+    chain.AddBlock("Block 1", "Alice", []*blockchain.Transaction{tx})
+    fmt.Println()
+
+    // Print the blockchain.
+    for _, block := range chain.Blocks {
+        fmt.Printf("Previous hash: %x\n", block.PrevHash)
+        fmt.Printf("Data: %s\n", block.Data)
+        fmt.Printf("Hash: %x\n", block.Hash)
+
+        pow := blockchain.NewProofOfWork(block)
+        fmt.Printf("IsValidPoW: %s\n", strconv.FormatBool(pow.Validate()))
+        fmt.Println()
+        
+        fmt.Println("Transactions:")
 
         for _, tx := range block.Transactions {
             fmt.Printf("Sender: %s\n", tx.Sender)
@@ -37,5 +71,5 @@ func main() {
             fmt.Println()
         }
         fmt.Println()
-	}
+    }
 }
